4d80ab006fd0e727adddad8943aa884734a2b04c
Lavanya <112009192+lav-user@users.noreply.github.com>
2024-05-01 13:40:21 -0700

    Update AssemblyInfo.cs

--- Parasoft.Dottest.Examples.Bank/Properties/AssemblyInfo.cs
+++ Parasoft.Dottest.Examples.Bank/Properties/AssemblyInfo.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+//adding sample comment
 [assembly: AssemblyTitle("Parasoft.Dottest.Examples.Bank")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
